9843189a0ea078f93e85870a06b3a4091e7acde3
biskyt <biskyt@users.noreply.github.com>
2020-10-06 18:58:08 +0000

    update port forward filename

--- portforward_watcher.sh
+++ portforward_watcher.sh
@@ -6,9 +6,9 @@ PORT=$(cat /portforward/port.dat)
 echo "Update port forward to: $PORT"
 transmission-remote http://localhost:9091/transmission -n $TRUSER:$TRPASSWD -p $PORT
 
-inotifywait -q -m -e close_write /portforward/port.txt |
+inotifywait -q -m -e close_write /portforward/port.dat |
 while read -r filename event; do
-  PORT=`cat /portforward/port.txt`
+  PORT=`cat /portforward/port.dat`
   echo "Update port forward to: $PORT"
   transmission-remote -n $TRUSER:$TRPASSWD -p $PORT
 done
